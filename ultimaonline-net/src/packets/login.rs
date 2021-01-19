49bc5f110a3c1eb358f9e3a54df6f32d45462a5d
Charles Ellis <hamled@hamled.dev>
2021-01-19 22:34:55 +0700

    ServerList packet implementation

--- ultimaonline-net/src/packets/login.rs
+++ ultimaonline-net/src/packets/login.rs
@@ -1,4 +1,6 @@
+use crate::types::FixedStr;
 use macros::packet;
+use serde::Serialize;
 use serde_repr::Serialize_repr;
 
 #[allow(dead_code)]
@@ -16,6 +18,22 @@ enum LoginRejectionReason {
 #[packet(id = 0x82)]
 struct LoginRejection {
     reason: LoginRejectionReason,
+}
+
+#[derive(Serialize)]
+#[repr(C, packed(1))]
+struct ServerInfo {
+    index: u16,
+    name: FixedStr<32>,
+    fullness: u8,
+    timezone: u8,
+    ip_address: u32,
+}
+
+#[packet(id = 0xA8, var_size = true)]
+struct ServerList {
+    flags: u8,
+    list: Vec<ServerInfo>,
 }
 
 #[cfg(test)]
@@ -39,6 +57,44 @@ mod tests {
             .expect("Failed to write packet");
 
             assert_eq!(packet.as_slice(), rej_invalid);
+        }
+    }
+
+    mod server_list {
+        use super::*;
+
+        #[test]
+        fn serialize() {
+            let servers = vec![
+                ServerInfo {
+                    index: 0,
+                    name: "Server 1".into(),
+                    fullness: 10,
+                    timezone: 3,
+                    ip_address: 0x12345678,
+                },
+                ServerInfo {
+                    index: 1,
+                    name: "Another Server".into(),
+                    fullness: 39,
+                    timezone: 9,
+                    ip_address: 0x09080706,
+                },
+            ];
+
+            let server_list = include_bytes!("../../test/resources/ServerList.pkt");
+
+            let mut packet = Vec::<u8>::new();
+            to_writer(
+                &mut packet,
+                &ServerList {
+                    flags: 0x5D,
+                    list: servers,
+                },
+            )
+            .expect("Failed to write packet");
+
+            assert_eq!(packet.as_slice(), server_list);
         }
     }
 }
