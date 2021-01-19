77f18de38f24ef1c33d4b1eccb0e811705b62a54
Charles Ellis <hamled@hamled.dev>
2021-01-19 22:25:17 +0700

    packet macro supports variable sized packets
    This implementation is incredibly brittle right now, in particular the
    part that generates code to calculate the size that a packet will have.
    
    Ideally we would serialize the packet and count the bytes from that,
    thus automatically relying upon the existing serialization logic.
    
    That's not really what serde wants us to do however, because it supports
    many different approaches to serialization, not just binary streams.
    
    So for the moment we do some hacky approach that special cases
    particular types that we know will serialize to a variable length (just
    Vec at the moment).

--- ultimaonline-net/macros/src/lib.rs
+++ ultimaonline-net/macros/src/lib.rs
@@ -1,11 +1,77 @@
 use darling::FromMeta;
 use proc_macro::{self, TokenStream};
+use proc_macro2::{self, Span};
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, parse_quote, AttributeArgs, ItemStruct};
+use syn::{
+    parse_macro_input, parse_quote, AttributeArgs, Field, Fields, GenericArgument, Ident,
+    ItemStruct, Path, PathArguments, PathSegment, Type,
+};
 
 #[derive(Debug, FromMeta)]
 struct PacketArgs {
     id: u8,
+    #[darling(default)]
+    var_size: bool,
+}
+
+fn packet_size_calc(item: &ItemStruct) -> proc_macro2::TokenStream {
+    let fields = match &item.fields {
+        Fields::Unnamed(fields) => &fields.unnamed,
+        Fields::Named(fields) => &fields.named,
+        Fields::Unit => unimplemented!("Cannot create a packet from unit struct"),
+    };
+
+    // HACK: All of this is super fragile
+    // we're trying to figure out if the type for a struct field
+    // is ::std::vec::Vec looking at the last segment of the path,
+    // looking for Vec with a single type argument
+    fn vec_type(path: &Path) -> Option<&Type> {
+        match path.segments.last() {
+            Some(PathSegment { ident, arguments }) => {
+                if *ident == Ident::new("Vec", Span::call_site()) {
+                    match arguments {
+                        PathArguments::AngleBracketed(args) => args
+                            .args
+                            .iter()
+                            .filter_map(|a| match a {
+                                GenericArgument::Type(ty) => Some(ty),
+                                _ => None,
+                            })
+                            .next(),
+                        _ => None,
+                    }
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn field_size_term(field: &Field) -> proc_macro2::TokenStream {
+        let id = &field.ident;
+        let ty = &field.ty;
+        match ty {
+            Type::Path(path) => {
+                if let Some(inner_type) = vec_type(&path.path) {
+                    // Add 2 for the field holding the length of th vector
+                    quote! { (2 + ::core::mem::size_of::<#inner_type>() * self.#id.len()) }
+                } else {
+                    quote! { ::core::mem::size_of::<#ty>() }
+                }
+            }
+            _ => quote! { ::core::mem::size_of::<#ty>() },
+        }
+    }
+
+    let terms = fields.iter().map(field_size_term);
+
+    let output = quote! {
+        let prefix_size = 1 + 2; // packet id and size field
+        let size = (prefix_size + #(#terms)+*) as u16;
+    };
+
+    output.into()
 }
 
 #[proc_macro_attribute]
@@ -22,6 +88,17 @@ pub fn packet(args: TokenStream, item: TokenStream) -> TokenStream {
 
     let main_struct = parse_macro_input!(item as ItemStruct);
     let main_ident = &main_struct.ident;
+
+    // If this packet has a variable size, generate code to
+    // calculate the size and include it when serializing
+    let (size_field_def, size_field, size_calc) = match args.var_size {
+        true => (
+            quote! {size: u16,},
+            quote! {size,},
+            packet_size_calc(&main_struct),
+        ),
+        false => (quote! {}, quote! {}, quote! {}),
+    };
 
     let impl_struct = ItemStruct {
         vis: parse_quote! { pub(self) },
@@ -42,6 +119,7 @@ pub fn packet(args: TokenStream, item: TokenStream) -> TokenStream {
         #[derive(::serde::Serialize)]
         struct #packet_ident<'a> {
             id: u8,
+            #size_field_def
             contents: &'a #impl_ident,
         }
 
@@ -50,9 +128,12 @@ pub fn packet(args: TokenStream, item: TokenStream) -> TokenStream {
             where
                 S: ::serde::ser::Serializer,
             {
+                #size_calc
+
                 let impl_ptr = self as *const #main_ident as *const #impl_ident;
                 let packet = #packet_ident {
                     id: #packet_id,
+                    #size_field
                     contents: unsafe { &*impl_ptr },
                 };
 
