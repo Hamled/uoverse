b651f8e8f64cf09cca5ffa78c92ccc618779a370
Charles Ellis <hamled@hamled.dev>
2021-03-21 07:16:14 +0700

    Packet structures for character list

--- ultimaonline-net/src/packets/char_select.rs
+++ ultimaonline-net/src/packets/char_select.rs
@@ -8,3 +8,40 @@ pub struct GameLogin {
     pub username: FixedStr<30>,
     pub password: FixedStr<30>,
 }
+
+#[packet(id = 0xB9)]
+pub struct Features {
+    pub flags: u32,
+}
+
+#[packet(id = 0xA9, var_size = true)]
+pub struct CharList {
+    pub chars: List<CharInfo, 8>,
+    pub cities: List<CityInfo, 8>,
+    pub flags: u32,
+    pub unknown_var1: i32,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct CharInfo {
+    pub name: FixedStr<30>,
+    pub unused: FixedStr<30>,
+}
+
+impl From<&str> for CharInfo {
+    fn from(val: &str) -> Self {
+        Self {
+            name: val.into(),
+            unused: Default::default(),
+        }
+    }
+}
+
+impl Default for CharInfo {
+    fn default() -> Self {
+        Self {
+            name: Default::default(),
+            unused: Default::default(),
+        }
+    }
+}
