eb4cf258c03008ab9f2a145a44972edf94d969cd
Charles Ellis <hamled@hamled.dev>
2022-07-16 19:44:42 -0700

    Server sends test mobile on enter world

--- uoverse-server/src/game/server.rs
+++ uoverse-server/src/game/server.rs
@@ -3,7 +3,10 @@ use std::sync::{
     Arc, Mutex,
 };
 use tokio::sync::mpsc;
-use ultimaonline_net::error::{Error, Result};
+use ultimaonline_net::{
+    error::{Error, Result},
+    types::Serial,
+};
 
 use super::client::{Client, ClientReceiver, ClientSender, WorldClient};
 
@@ -11,6 +14,8 @@ pub struct Server {
     shutdown: Arc<AtomicBool>,
     clients: Mutex<Vec<WorldClient>>,
 }
+
+const PLAYER_SERIAL: Serial = 3833;
 
 impl Server {
     pub fn new(shutdown: Arc<AtomicBool>) -> Self {
@@ -39,18 +44,89 @@ impl Server {
         let (input_send, input_recv) =
             mpsc::unbounded_channel::<<WorldClient as ClientReceiver>::RecvItem>();
 
+        let mut client = WorldClient {
+            sender: output_send,
+            receiver: input_recv,
+        };
+
+        self.enter_world(&mut client)?;
+
         self.clients
             .lock()
             .map_err(|_| Error::Message("Unable to lock clients vec".to_string()))?
-            .push(WorldClient {
-                sender: output_send,
-                receiver: input_recv,
-            });
+            .push(client);
 
         Ok(Client {
             sender: input_send,
             receiver: output_recv,
         })
     }
-}
+
+    fn enter_world(&self, client: &mut WorldClient) -> Result<()> {
+        use ultimaonline_net::{packets::*, types};
+
+        client.send(
+            mobile::MobLightLevel {
+                serial: PLAYER_SERIAL,
+                level: 30,
+            }
+            .into(),
+        )?;
+
+        client.send(world::WorldLightLevel { level: 30 }.into())?;
+
+        let mob_x = 3668;
+        let mob_dir = types::Direction::East;
+        client.send(
+            mobile::Appearance {
+                state: mobile::State {
+                    serial: 55858,
+                    body: 401,
+                    x: mob_x,
+                    y: 2625,
+                    z: 0,
+                    direction: mob_dir,
+                    hue: 1003,
+                    flags: mobile::EntityFlags::None,
+                    notoriety: types::Notoriety::Ally,
+                },
+                items: vec![
+                    mobile::Item {
+                        serial: 0x40000001,
+                        type_id: 0x1EFD, // Fancy Shirt
+                        layer: 0x05,     // Shirt
+                        hue: 1837,
+                    },
+                    mobile::Item {
+                        serial: 0x40000002,
+                        type_id: 0x1539, // Long Pants
+                        layer: 0x04,     // Pants
+                        hue: 1897,
+                    },
+                    mobile::Item {
+                        serial: 0x40000003,
+                        type_id: 0x170B, // Boots
+                        layer: 0x04,     // Shoes
+                        hue: 1900,
+                    },
+                    mobile::Item {
+                        serial: 0x40000004,
+                        type_id: 0x1515, // Cloak
+                        layer: 0x14,     // Cloak
+                        hue: 1811,
+                    },
+                    mobile::Item {
+                        serial: 0x40000005,
+                        type_id: 0x203C, // Long hair
+                        layer: 0x0B,     // Hair
+                        hue: 1111,
+                    },
+                ]
+                .into(),
+            }
+            .into(),
+        )?;
+
+        Ok(())
+    }
 }
