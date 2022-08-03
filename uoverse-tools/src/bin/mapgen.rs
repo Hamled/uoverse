b2d93e9112681a608f81ad61f7cf09472ef63638
Charles Ellis <hamled@hamled.dev>
2022-08-03 15:57:30 -0700

    mapgen generates hard-coded custom map
    This is a simple 256x256 map with all tiles as grass.

--- uoverse-tools/src/bin/mapgen.rs
+++ uoverse-tools/src/bin/mapgen.rs
@@ -1,16 +1,38 @@
-use std::{fs, io::Cursor};
-use uoverse_tools::archive::uo_package::UOPackage;
+use std::{convert::TryInto, fs::OpenOptions, io::Write};
+use uoverse_tools::{
+    archive::uo_package::UOPackage,
+    map::{Metadata, Tile, UOMap},
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let contents = fs::read("/home/charles/games/uo/client-classic/MainMisc.uop")?;
+    let prefix = "build/map6legacymul";
 
-    let mut reader = Cursor::new(contents.as_slice());
-    let package = UOPackage::new(&mut reader)?;
-
-    dbg!(&package);
-    for file in package.files {
-        fs::write(format!("./{:016X}.dat", file.hash), file.contents)?;
+    let mut map = UOMap::new(256, 256)?;
+    for x in 0..256 {
+        for y in 0..256 {
+            map.set(
+                x,
+                y,
+                Tile {
+                    kind: 0x0004,
+                    height: 0x00,
+                },
+            )?;
+        }
     }
+
+    let files = map.into_files(prefix)?;
+    let package: UOPackage = files.try_into()?;
+    dbg!(&package);
+
+    let mut package_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("map6LegacyMUL.uop")?;
+
+    package.write(&mut package_file)?;
+    package_file.flush()?;
 
     Ok(())
 }
